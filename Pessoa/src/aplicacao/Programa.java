2a116c3d5a47381f4ca7ef74713df73d51f0927c
Tailan de Souza <101639851+Naliat@users.noreply.github.com>
2022-08-03 09:10:19 -0300

    Add files via upload

---
+++ Pessoa/src/aplicacao/Programa.java
@@ -0,0 +1,20 @@
+package aplicacao;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+	Pessoa p1 = new Pessoa(1, "Daniel","daniel@gmail.com");
+	Pessoa p2 = new Pessoa(1, "Lucas","lucas@gmail.com");
+	Pessoa p3 = new Pessoa(1, "Ana","ana@gmail.com");
+	
+	
+	System.out.println(p1);
+	System.out.println(p2);
+	System.out.println(p3);
+	
+	
+	}
+
+}
